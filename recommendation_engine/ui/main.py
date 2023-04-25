f3a1f853298ed646f5d1abba9824e5480f3566ee
project-vending <131410219+project-vending@users.noreply.github.com>
2023-04-25 18:40:19 -0400

    Add main.py

---
+++ recommendation_engine/ui/main.py
@@ -0,0 +1,11 @@
+python
+# main.py
+
+import streamlit as st
+
+def main():
+    st.title("Recommendation Engine")
+    st.write("Welcome to the recommendation engine!")
+    
+if __name__ == "__main__":
+    main()
