ed3dd4103fa99a5c8b472dff762922c87ac3f3c8
project-vending <131410219+project-vending@users.noreply.github.com>
2023-04-25 18:40:21 -0400

    Add __init__.py

---
+++ recommendation_engine/src/__init__.py
@@ -0,0 +1,10 @@
+
+# recommendation_engine/src/__init__.py
+
+# Import modules
+from . import data_loader
+from . import data_transformer
+from . import recommender
+
+# Perform any package-level initialization
+# ...
